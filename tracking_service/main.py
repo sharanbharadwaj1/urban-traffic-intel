5cb67cce9a7ea58bb909229de58a99e174ebfcb6
Sharan Bharadwaj M A <sharanbharadwaj1@gmail.com>
2026-07-05 13:47:36 +0530

    code cleanup

--- tracking_service/main.py
+++ tracking_service/main.py
@@ -44,32 +44,3 @@ for msg_id, message in reader.read():
 
 
 
-# from stream_reader import StreamReader
-# from tracker import Tracker
-# from publisher import Publisher
-# import config
-
-# reader = StreamReader(
-#     config.REDIS_HOST,
-#     config.REDIS_PORT,
-#     config.INPUT_STREAM
-# )
-
-# tracker = Tracker()
-
-# publisher = Publisher(
-#     config.REDIS_HOST,
-#     config.REDIS_PORT,
-#     config.OUTPUT_STREAM
-# )
-
-# for message in reader.read():
-
-#     detections = message["detections"]
-
-#     tracks = tracker.update(detections)
-
-#     publisher.publish({
-#         "frame_id": message["frame_id"],
-#         "tracks": str(tracks)
-#     })
--- tracking_service/stream_reader.py
+++ tracking_service/stream_reader.py
@@ -31,30 +31,3 @@ class StreamReader:
                     yield msg_id, data
 
 
-# import redis
-# import json
-
-# class StreamReader:
-
-#     def __init__(self, host, port, stream):
-
-#         self.redis = redis.Redis(host=host, port=port)
-#         self.stream = stream
-#         self.last_id = "0"
-
-#     def read(self):
-
-#         response = self.redis.xread(
-#             {self.stream: self.last_id},
-#             block=0
-#         )
-
-#         for stream, messages in response:
-
-#             for msg_id, msg in messages:
-
-#                 self.last_id = msg_id
-
-#                 data = json.loads(msg[b'payload'])
-
-#                 yield data
